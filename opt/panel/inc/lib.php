981cc5723d81d420f5b3288426f3e82b4882277e
SirDankenstien <52797753+SirDankenstien@users.noreply.github.com>
2020-11-27 22:53:28 +0530

    Update lib.php
    Added Paper 1.16.4

--- opt/panel/inc/lib.php
+++ opt/panel/inc/lib.php
@@ -541,6 +541,7 @@ log: ".$_POST['dir']."/ngrok.log \n
 	// Accept eula
 	file_put_contents($_POST['dir'].'/eula.txt','eula=TRUE');
 	// Copy spigot
+	copy('serverbase/server-icon.png',$_POST['dir'].'/server-icon.png');
 	if ($_POST['version'] == '1.10.2') {
 		copy('serverbase/spigot-1.10.2.jar',$_POST['dir'].'/spigot-1.10.2.jar');
 	} else if ($_POST['version'] == '1.11.2') {
@@ -549,6 +550,10 @@ log: ".$_POST['dir']."/ngrok.log \n
 		copy('serverbase/spigot-1.12.2.jar',$_POST['dir'].'/spigot-1.12.2.jar');
 	} else if ($_POST['version'] == '1.14.4') {
 		copy('serverbase/spigot-1.14.4.jar',$_POST['dir'].'/spigot-1.14.4.jar');
+	} else if ($_POST['version'] == '1.15.2') {
+		copy('serverbase/spigot-1.15.2.jar',$_POST['dir'].'/spigot-1.15.2.jar');
+	} else if ($_POST['version'] == '1.16.4') {
+		copy('serverbase/paper-1.16.4.jar',$_POST['dir'].'/paper-1.16.4.jar');
 	} else if ($_POST['version'] == '1.9.4') {
 		copy('serverbase/spigot-1.9.4-R0.1-SNAPSHOT-latest.jar',$_POST['dir'].'/spigot-1.9.4.jar');
 	} else if ($_POST['version'] == '1.8.8') {
